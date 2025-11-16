4bf8f0fc19591cdd514bfae770a26fff149e7123
Jorge Sanchez <jorgeluissanchezbarreneche@gmail.com>
2025-11-15 20:33:22 -0500

    fix: update ENABLE_API_MOCKING schema to be optional with a default value

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,9 +6,10 @@ const createEnv = () => {
     API_URL: z.string(),
     ENABLE_API_MOCKING: z
       .string()
+      .optional()
+      .default("false")
       .refine((s) => s === "true" || s === "false")
-      .transform((s) => s === "true")
-      .optional(),
+      .transform((s) => s === "true"),
     APP_URL: z.string().optional().default("http://localhost:3000"),
     APP_MOCK_API_PORT: z.string().optional().default("8080"),
   });
